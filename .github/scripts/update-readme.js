f4c39a748b5e180f3221814c33556c183fdb76e8
Andrew Grizhenkov - BJSS <86948263+AndrewGrizhenkov@users.noreply.github.com>
2024-04-24 08:38:07 +0100

    Create update-readme.js

---
+++ .github/scripts/update-readme.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const axios = require('axios');
+
+async function getTopContributors(AndrewGrizhenkov, copilot-metrics-viewer) {
+    const { data } = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contributors`);
+    return data.slice(0, 10).map(contributor => ({
+        login: contributor.login,
+        contributions: contributor.contributions
+    }));
+}
+
+async function updateReadme(owner, repo) {
+    const contributors = await getTopContributors(owner, repo);
+    const readme = fs.readFileSync('README.md', 'utf8');
+    const updatedReadme = readme.replace(/## Top Contributors\n\n([^#]+)/, `## Top Contributors\n\n${contributors.map(contributor => `1. [${contributor.login}](https://github.com/${contributor.login}) - ${contributor.contributions} commits`).join('\n')}\n`);
+    fs.writeFileSync('README.md', updatedReadme);
+}
+
+updateReadme('owner', 'repo');
